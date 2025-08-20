a71f47f2407e8ab6c233bdc29cea76ca4564c0ee
Jay19wool90way <131035590+Jay19wool90way@users.noreply.github.com>
2025-08-20 13:43:38 +0100

    Check and fix analyze-photo function

--- supabase/functions/analyze-photo/index.ts
+++ supabase/functions/analyze-photo/index.ts
@@ -1,10 +1,4 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
-import { createClient } from 'npm:@supabase/supabase-js@2.49.1';
-
-const supabase = createClient(
-  Deno.env.get('SUPABASE_URL') ?? '',
-  Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
-);
 
 const googleApiKey = 'AIzaSyB5O-KiDzFSeZP9jvpemQZUhRwla9lagLQ';
 
